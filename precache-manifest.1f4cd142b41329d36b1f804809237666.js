2ca30708f38b92cf976f62c5634365340d05a996
Alvin Mantovani <alvin.ictn@gmail.com>
2020-11-01 23:58:54 +0700

    init demo

---
+++ precache-manifest.1f4cd142b41329d36b1f804809237666.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119f29906a935cbc04719aa0b5b9cd70",
+    "url": "/demo-Vet/index.html"
+  },
+  {
+    "revision": "62c8b94056f258168a30",
+    "url": "/demo-Vet/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0290656859c7ea0cf221",
+    "url": "/demo-Vet/static/css/main.75ff2e09.chunk.css"
+  },
+  {
+    "revision": "62c8b94056f258168a30",
+    "url": "/demo-Vet/static/js/2.f5c3f67c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/demo-Vet/static/js/2.f5c3f67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0290656859c7ea0cf221",
+    "url": "/demo-Vet/static/js/main.996f13d2.chunk.js"
+  },
+  {
+    "revision": "e4e4a91e10efffa4eb64",
+    "url": "/demo-Vet/static/js/runtime-main.1a33d5fb.js"
+  },
+  {
+    "revision": "35a8f62f895c973fb6a4a2da0769f8ea",
+    "url": "/demo-Vet/static/media/Cat.35a8f62f.svg"
+  },
+  {
+    "revision": "1ce078a8e4658d846e4d658123c54b14",
+    "url": "/demo-Vet/static/media/Doc.1ce078a8.svg"
+  },
+  {
+    "revision": "7fdbc35fdc533eef2982cf184b797e2c",
+    "url": "/demo-Vet/static/media/Dog.7fdbc35f.svg"
+  },
+  {
+    "revision": "c5c3f62f59c6eefed49fbf0385e4d8db",
+    "url": "/demo-Vet/static/media/Vector 2.c5c3f62f.svg"
+  },
+  {
+    "revision": "c48948ddf19c0d3ce81c4615449e3045",
+    "url": "/demo-Vet/static/media/Vector 3.c48948dd.svg"
+  },
+  {
+    "revision": "b92112b8890a0286f0d89674dcff07de",
+    "url": "/demo-Vet/static/media/Vector 4.b92112b8.svg"
+  },
+  {
+    "revision": "a1c99fde1466bc4f8fd6e7cbdd15324a",
+    "url": "/demo-Vet/static/media/klinik.a1c99fde.png"
+  },
+  {
+    "revision": "3b128daffcf6b9f693ea5fbe65c50632",
+    "url": "/demo-Vet/static/media/loginimage.3b128daf.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/demo-Vet/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "fe720a5471715643d956a4340e2d616b",
+    "url": "/demo-Vet/static/media/register-lock.fe720a54.svg"
+  },
+  {
+    "revision": "7a628f6df58fe6a1073ce327e69eadf8",
+    "url": "/demo-Vet/static/media/register-mail.7a628f6d.svg"
+  },
+  {
+    "revision": "83a84dff37d5578604a2c18240f1efc8",
+    "url": "/demo-Vet/static/media/register-phone.83a84dff.svg"
+  },
+  {
+    "revision": "4fd93a68d0d7d0c68dd92a2bfbdf6c2e",
+    "url": "/demo-Vet/static/media/register-user.4fd93a68.svg"
+  },
+  {
+    "revision": "05dae58c201c99765f0deaa8441d7d6e",
+    "url": "/demo-Vet/static/media/rs.05dae58c.png"
+  }
+]);
